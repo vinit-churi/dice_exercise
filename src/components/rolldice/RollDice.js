a0bf445be2f76b84edf00fe1429e89a32e6daf8d
vinit-churi <vinitchuri@hotmail.com>
2020-07-08 20:11:27 +0530

    :tada:finaly done with the udemy dice shuffle exercise without animation

--- src/components/rolldice/RollDice.js
+++ src/components/rolldice/RollDice.js
@@ -4,12 +4,51 @@ import './RollDice.css';
 
 
 class RollDice extends Component {
+    constructor(props){
+        super(props);
+        this.state ={
+            diceOne: 'one',
+            diceTwo: 'two'
+        }
+        this.shuffleDice = this.shuffleDice.bind(this);
+    }
+
+
+    randomdice(){
+        const num= Math.floor(Math.random() * 6) + 1;
+
+        switch (num) {
+            case 1 :
+                return 'one';
+            case 2:
+                return 'two';
+            case 3:
+                return 'three';
+            case 4:
+                return 'four';
+            case 5:
+                return 'five';
+            case 6:
+                return 'six';
+            default:
+                return 'one';
+        }
+        
+    };
+
+    shuffleDice() {
+        this.setState({
+            diceOne: this.randomdice(),
+            diceTwo: this.randomdice(),
+        })
+    }
+
     render() {
-        let dface='two'
         return(
             <div className='Rolldice'>
-
-                <Dice icon={`dice-${dface}`}/>
+                <Dice icon={`dice-${this.state.diceOne}`}/>
+                <Dice icon={`dice-${this.state.diceTwo}`}/>
+                <button className='Rolldice__button' onClick={this.shuffleDice}>shuffle</button>
             </div>
         )
     }
